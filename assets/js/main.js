a53a0d209530a1c431719f141990f690fa285bc0
Viktor Casubolo <viktor.casubolo98@gmail.com>
2021-03-14 17:20:16 +0100

    Esercizio completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,48 @@
-var hamburgerMenu = $('.hamburger-menu');
-var hamburgerIconClose = $('.header-container i');
-var hamburgerIcon = $('.hamburger-icon');
+var containerFeatures =$('.container-features');
+var containerFeaturesXs =$('.container-features-xs');
+var linkHover = $('a.link-hover');
+var linkHoverXs = $('a.link-hover-xs');
+var hamburger = $('.nav-bar-xs i');
+var navBarXs = $('.nav-bar-xs');
+var cross = $('.hamburger-dropdown i');
+var hamburgerDropdown = $('.hamburger-dropdown');
+var linksDropdown = $('.links-drop');
 
 $(document).ready(function() {
-    hamburgerIcon.click(function(){
-        hamburgerMenu.show();
-    });
-
-    hamburgerIconClose.click(function(){
-        hamburgerMenu.hide();
-    })
+    mouseHover();
+    mouseClick();
+    mouseClickXs();
+    
 });
 
+function mouseHover(){
+    linkHover.mouseenter(function(){
+        $(this).next().addClass('active');
+        console.log('ok');
+        
+    });
+
+    containerFeatures.mouseleave(function(){
+        linkHover.next().removeClass('active');
+        
+    });
+};
+
+function mouseClick(){
+    hamburger.click(function(){
+        hamburgerDropdown.addClass('active');
+        linksDropdown.addClass('links-active');
+        navBarXs.hide();
+    })
+    cross.click(function(){
+        linksDropdown.removeClass('links-active');
+        navBarXs.show();
+    })
+}
+
+function mouseClickXs(){
+    linkHoverXs.click(function(){
+        $(this).next().toggleClass('active');
+        
+    });
+};
